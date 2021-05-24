b7f0c48971f4b41e4e2ce30879864c1f5db82949
Александр <84118981+ASevastyanov@users.noreply.github.com>
2021-05-24 23:19:23 +0600

    Урок-5
    Улучшение селекторов CSS/XPath checker

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -9,16 +9,31 @@ class FirstCest
     // Тест на проверку товара blouse
     public function tryToTest(AcceptanceTester $I)
     {
-        
+        $searchTitleCss = 'a[title="Blouse"]';
+        $searchTitleXPath = '//a[@title="Blouse"]';
+        $moveMouseCss = '#homefeatured img[title="Blouse"]';
+        $moveMouseXPath  = '//*[@id="homefeatured"]//img[@title="Blouse"]'; //двойные ковычки спользовал так как ругается PHPStorm
+        $clickButtonCss = '#homefeatured>li:nth-child(2) a.quick-view';
+        $clickButtonXPath  = '//*[@id="homefeatured"]/li[2]//a[@class="quick-view"]';
+        $waitForElementCss = '.fancybox-inner';
+        $waitForElementXPath  = '//*[@class="fancybox-inner"]';
+        $switchToIframeCss = '.fancybox-inner';
+        $switchToIframeXPath  = '//*[@class="fancybox-inner"]';
+        $waitForInternetCss = '#product';
+        $waitForInternetXPath  = '//*[@id="product"]';
+        $seeTextCss = '#product h1';
+        $seeTextXPath  = '//*[@id="product"]//h1';
 
-        $I->amOnPage('');
-        $I->seeElement('a[title="Blouse"]');
-        $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
-        $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
-        $I->waitForElement('.fancybox-inner');
-        $I->switchToIframe('.fancybox-iframe');
-        $I->see('Blouse','.pb-center-column > h1');
 
+        //Так же сразу переделал тест на правильный CSS
+         $I->amOnPage('');
+         $I->seeElement('#homefeatured a[title="Blouse"]');
+         $I->moveMouseOver('#homefeatured img[title="Blouse"]');
+         $I->click('#homefeatured>li:nth-child(2) a.quick-view');
+         $I->waitForElement('.fancybox-inner');
+         $I->switchToIframe('.fancybox-iframe');
+         $I->waitForElement('#product'); //было добавлено время ожидания. Так как тест валился из за соеденения интернета
+         $I->see('Blouse','#product h1');
 
 
 
--- tests/functional/SecondCest.php
+++ tests/functional/SecondCest.php
@@ -9,6 +9,16 @@ class SecondCest
     // Тест на проверку поиска
     public function test(AcceptanceTester $I)
     {
+        $searchTitleCss = '#search_query_top';
+        $searchTitleXPath = '//*[@id="search_query_top"]';
+        $fillFieldCss = '#search_query_top';
+        $fillFieldXPath = '//*[@id="search_query_top"]';
+        $clickButtonCss = '#searchbox > button';
+        $clickButtonXPath  = '//*[@id="searchbox"]/button';
+        $seeNumberOfElementsCss = '#center_column > ul >li '; //тут переделал по причини,  что не удалось реализовать по ul.product_list>li
+        $seeNumberOfElementsXPath  = '//*[@id="center_column"]/ul/li';
+
+
     $I->amOnPage('');
     $I->seeElement('#search_query_top');
     $I->fillField('#search_query_top','Printed dress');
